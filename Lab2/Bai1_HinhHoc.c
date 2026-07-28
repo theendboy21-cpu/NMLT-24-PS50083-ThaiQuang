ed8a5e058090d48bc5954499fbff0fb730459064
theendboy21-cpu <theendboy21@gmail.com>
2026-07-28 20:50:52 +0700

    cap nhat code

--- Lab2/Bai1_HinhHoc.c
+++ Lab2/Bai1_HinhHoc.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <math.h>
+#define PI 3.14159
 
-int main() {   
-    
-    float a, b, c, p, S;    
-    printf("Nhap do dai 3 canh cua tam giac: ");
-    scanf("%f %f %f", &a, &b, &c);
+int main()
+{
+    double chieuDai, chieuRong;
+    double cvHcn, dtHcn;
+    printf("Nhap chieu dai: ");
+    scanf("%lf", &chieuDai);
+    printf("Nhap chieu rong: ");
+    scanf("%lf", &chieuRong);
+    cvHcn = (chieuDai + chieuRong) * 2;
+    dtHcn = chieuDai * chieuRong;
 
-    
-    p = (a + b + c) / 2;
-    S = sqrt(p * (p - a) * (p - b) * (p - c));
-    
-    printf("Chu vi tam giac la: %.2f\n", p);
-    printf("Dien tich tam giac la: %.2f\n", S);
+    printf("Chu vi HCN: %.2f\n", cvHcn);
+    printf("Dien tich HCN: %.2f\n\n", dtHcn);
+
+    double bk;
+    double cvTron, dtTron;
+
+    printf("Nhap ban kinh: ");
+    scanf("%lf", &bk);
+    cvTron = 2 * PI * bk;
+    dtTron = PI * bk * bk;
+    printf("Chu vi Hinh Tron: %.5f\n", cvTron);
+    printf("Dien tich Hinh Tron: %.5f\n", dtTron);
+
     return 0;
 }
