fc2bf2d2faad688dcf00f8753ebeff9c1a670989
theendboy21-cpu <theendboy21@gmail.com>
2026-08-08 21:38:10 +0700

    cap nhat code

--- siles8/demosile8.c
+++ siles8/demosile8.c
@@ -1,44 +1,64 @@
 #include <stdio.h>
+
+struct NgayThang
+{
+   int ngay;
+   int thang;
+   int nam;
+};
+
+struct SinhVien
+{
+   char maSV[8];
+   char tenSV[30];
+   float diemSV;
+   struct NgayThang ngaySinh;
+};
+
 int main()
 {
-    int n=1;
-    char dsMaSV[40, 8];
-    char dstenSV[40, 30];
-    float dsDiemSV[40];
-    // sap xep ds sv theo diem => thay doi vitri cua dsMaSV, dstenSV
-    struct NgayThang
-    {
-       int ngay;
-       int thang;
-       int nam;
-    };
-    
-    struct SinhVien{
-          char maSV[8];
-        char tenSV[30];
-        float diemSV;
-        struct NgayThang ngaySinh;
-      
-        
-    }
-    
-    struct SinhVien dsSinhVien[40];
-    {
-       for(i=1;i<n;i++){
-        printf("nhap ma so sinh vien [%d]:",i);
-        gets(dsSinhVien[i].maSV);
-        printf("Nhap ten Sinh vien[%d]",i);
-        gets(dsSinhVien[i].tenSV);
-        printf("nhap diem so sinh vien[%d]",i);
-        scanf("%f",&dsSinhVien.diemSV)
+   int n = 1;
+   struct SinhVien dsSinhVien[40];
 
-        getchar();
-       }
-    };
-    printf("Danh sach sinh vien: \n");
-    printf("MaSV  TenSV       Diem SV");
-    for(int i=00;i<n;i++){
-         
-    }
-    return 0;
+   printf("Nhap so luong sinh vien: ");
+   scanf("%d", &n);
+   getchar();
+
+   // Vòng lặp nhập thông tin
+   for (int i = 0; i < n; i++)
+   {
+      printf("Nhap thong tin sinh vien [%d]:\n", i + 1);
+
+      printf("Nhap ma so sinh vien: ");
+      gets(dsSinhVien[i].maSV);
+
+      printf("Nhap ten sinh vien: ");
+      gets(dsSinhVien[i].tenSV);
+
+      printf("Nhap ngay sinh (ngay thang nam cach nhau bang khoang trang): ");
+      scanf("%d %d %d", &dsSinhVien[i].ngaySinh.ngay,
+            &dsSinhVien[i].ngaySinh.thang,
+            &dsSinhVien[i].ngaySinh.nam);
+
+      printf("Nhap diem so sinh vien: ");
+      scanf("%f", &dsSinhVien[i].diemSV);
+
+      getchar();
+   }
+
+   // Xuat ra man hinh
+   printf("%-10s %-25s %-10s %-15s\n", "MaSV", "TenSV", "Diem SV", "Ngay Sinh");
+
+   for (int i = 0; i < n; i++)
+   {
+      printf("%-10s %-25s %-10.2f %02d-%02d-%-9d\n",
+             dsSinhVien[i].maSV,
+             dsSinhVien[i].tenSV,
+             dsSinhVien[i].diemSV,
+             dsSinhVien[i].ngaySinh.ngay,
+             dsSinhVien[i].ngaySinh.thang,
+             dsSinhVien[i].ngaySinh.nam);
+   }
+
+   return 0;
 }
