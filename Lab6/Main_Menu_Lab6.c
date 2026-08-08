92dd5ff3a91a015c6217bf4d81ad9e3682326ea2
theendboy21-cpu <theendboy21@gmail.com>
2026-08-08 12:04:11 +0700

    cap nhat code

--- Lab6/Main_Menu_Lab6.c
+++ Lab6/Main_Menu_Lab6.c
@@ -150,36 +150,42 @@ int main()
         switch (chon)
         {
         case 1:
+            printf("Ban da chon chuc nang 1: Tinh TBC cac so chia het cho 3 va 5.\n");
             printf("Nhap so luong phan tu n = ");
             scanf("%d", &n);
             tinhTrungBinhCong(a, n);
             break;
 
         case 2:
+            printf("Ban da chon chuc nang 2: Tim Min Max trong mang.\n");
             printf("Nhap so luong phan tu n = ");
             scanf("%d", &n);
             timMaxMin(a, n);
             break;
 
         case 3:
+            printf("Ban da chon chuc nang 3: Sap xep mang theo thu tu giam dan.\n");
             printf("Nhap so luong phan tu n = ");
             scanf("%d", &n);
             sapXepGiamDan(a, n);
             break;
 
         case 4:
+            printf("Ban da chon chuc nang 4: Ma tran binh phuong.\n");
             printf("Nhap so hang (r) va so cot (c) cua ma tran: ");
             scanf("%d %d", &r, &c);
             maTranBinhPhuong(mt, r, c);
             break;
 
         case 5:
+            printf("Ban da chon chuc nang 5: Loc va xuat cac so le ma tran.\n");
             printf("Nhap so hang (r) va so cot (c) cua ma tran: ");
             scanf("%d %d", &r, &c);
             locSoLeMaTran(mt, r, c);
             break;
 
         case 6:
+            printf("Ban da chon chuc nang 6: Thoat chuong trinh.\n");
             printf("Tam biet!\n");
             break;
 
